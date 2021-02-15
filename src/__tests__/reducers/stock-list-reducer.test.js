92a8abee1f3baafb60db99e0e6c6c4f03aee3b54
Eric Stratton <strattonericj@gmail.com>
2021-02-15 09:50:27 -0800

    good fail second test
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- src/__tests__/reducers/stock-list-reducer.test.js
+++ src/__tests__/reducers/stock-list-reducer.test.js
@@ -1,7 +1,38 @@
 import stockListReducer from '../../reducers/stock-list-reducer';
 
 describe ('stockListReducer', () => {
+
+  let action;
+    const stockData = {
+      name: 'T-Shirt',
+      description: 'Red t-shirts.',
+      quantity: '20',
+      id: 1
+    };
+
   test('Should return default state if there is no action passed into reducer', () => {
     expect(stockListReducer({}, { type: null })).toEqual({});
   });
+
+  
+  test('Should succesfully add new stock data to masterStockList', () => {
+    const { name, description, quantity, id } = stockData;
+    action = {
+      type: 'ADD_STOCK',
+      name: name, 
+      description: description,
+      quantity: quantity,
+      id: id
+    };
+
+    expect(stockListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        description: description,
+        quantity: quantity,
+        id: id
+      }
+    });
+  });
+
 });
